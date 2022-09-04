0a3121ed665ad2987c28b8e7f94efc6f69ecc2f4
Hyewon Choi <dearhw117@naver.com>
2022-09-04 23:52:57 +0900

    0904

---
+++ quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+    {
+        quote : "The way to get started is to quit talking and begin doign.",
+        author : "walt disney",
+    },
+    {
+        quote : "2The way to get started is to quit talking and begin doign.",
+        author : "walt disney",
+    },
+    {
+        quote : "3The way to get started is to quit talking and begin doign.",
+        author : "walt disney",
+    }
+];
+
+const quote = document.querySelector("#quote span:fisrt-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0]);
