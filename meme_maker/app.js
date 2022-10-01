fb017506f64b14c958d83e23058f25884854d1e8
Hyewon Choi <dearhw117@naver.com>
2022-10-01 23:33:38 +0900

    1001

--- meme_maker/app.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.width=800;
-canvas.height=800;
-
-ctx.fillRect(50,50,100,40);
-ctx.strokeRect(200,300, 100,30)
