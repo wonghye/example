2b5a55fe3f42619307f134de8420beb6c5e8c057
Hyewon Choi <dearhw117@naver.com>
2022-10-03 21:49:10 +0900

    1003

--- meme_maker/app2.js
+++ meme_maker/app2.js
@@ -4,11 +4,15 @@ const ctx = canvas.getContext("2d");
 canvas.width=800;
 canvas.height=800;
 
-ctx.rect(150,150,100,100);
-ctx.rect(250,250,100,100);
-ctx.fill();
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ ctx.fillRect(260 - 40, 200 - 30, 60, 200);
 
-ctx.beginPath();
-ctx.rect(350,350,100,100);
-ctx.fillStyle="yellow";
+ ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ ctx.fill();
+
+ ctx.beginPath();
+ ctx.fillStyle = "white";
+ ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
